f61d8b83d104e50d306059d0a026a757ab4d0c04
cristopherleojs <cristopherleonardo96@gmail.com>
2026-02-27 10:58:02 -0600

    feat: Se agregaron nuevas importaciones en AppDatabase

--- app/src/main/java/com/example/medicare/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/medicare/data/local/database/AppDatabase.kt
@@ -4,18 +4,36 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+
+// Nuevas Importaciones de las entidades (Tablas)
 import com.example.medicare.data.local.entity.Usuario
 import com.example.medicare.data.local.entity.Enfermedad
+import com.example.medicare.data.local.entity.Medicamento
+import com.example.medicare.data.local.entity.Programacion
+import com.example.medicare.data.local.entity.HistorialToma
+
+// Nuevas importaciones, TODOS los DAOs (Consultas)
 import com.example.medicare.data.local.dao.UsuarioDao
 import com.example.medicare.data.local.dao.EnfermedadDao
+import com.example.medicare.data.local.dao.MedicamentoDao
+import com.example.medicare.data.local.dao.ProgramacionDao
+import com.example.medicare.data.local.dao.HistorialTomaDao
 
-@Database(entities = [Usuario::class, Enfermedad::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        Usuario::class,
+        Enfermedad::class,
+        Medicamento::class,
+        Programacion::class,
+        HistorialToma::class
+    ],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun usuarioDao(): UsuarioDao
     abstract fun enfermedadDao(): EnfermedadDao
-
-    // nuevos Daos o tablas
     abstract fun medicamentoDao(): MedicamentoDao
     abstract fun programacionDao(): ProgramacionDao
     abstract fun historialTomaDao(): HistorialTomaDao
